3f42c73c4a3393d4dab9ccc352f3d414a3fa5b56
Samuel Callu <Samcallwind@gmail.com>
2025-09-17 17:54:31 +0200

    Added the log.info for Medical records

--- src/main/java/com/openclassrooms/safetynet/controller/MedicalRecordController.java
+++ src/main/java/com/openclassrooms/safetynet/controller/MedicalRecordController.java
@@ -3,9 +3,11 @@ package com.openclassrooms.safetynet.controller;
 import com.openclassrooms.safetynet.dto.NameDto;
 import com.openclassrooms.safetynet.model.Medicalrecord;
 import com.openclassrooms.safetynet.service.MedicalRecordService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 public class MedicalRecordController {
 
@@ -14,16 +16,19 @@ public class MedicalRecordController {
 
     @PostMapping("/medicalRecord")
     public void addMedicalRecord(@RequestBody Medicalrecord medicalrecord) {
+        log.info("POST method received for the medical record: {}", medicalrecord);
         medicalRecordService.addMedicalRecord(medicalrecord);
     }
 
     @DeleteMapping("/medicalRecord")
     public void deleteMedicalRecord(@RequestBody NameDto dto) {
+        log.info("DELETE method received for the person: {}", dto);
         medicalRecordService.removeMedicalRecord(dto.getFirstName(), dto.getLastName());
     }
 
     @PutMapping("/medicalRecord")
     public void modifyMedicalRecord(@RequestBody Medicalrecord medicalrecord) {
+        log.info("PUT method received for the medical record: {}", medicalrecord);
         medicalRecordService.modifyMedicalRecord(medicalrecord);
     }
 }
